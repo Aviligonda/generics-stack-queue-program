5343cb58ee60342a445df206ff7f22b269a6522a
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-20 08:16:16 +0530

    Added : Adding elements to stack list

--- StackAndQueueList.java
+++ StackAndQueueList.java
@@ -1,5 +1,10 @@
 public class StackAndQueueList {
     public static void main(String[] args) {
         System.out.println("Welcome to Stack And Queue List  Program");
+        FunctionClass functionClass = new FunctionClass();
+        functionClass.insert(70);
+        functionClass.insert(30);
+        functionClass.insert(56);
+        functionClass.toPrint();
     }
 }
