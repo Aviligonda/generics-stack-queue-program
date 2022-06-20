3737e188ce33f8381eed710fc33433599b4c88e3
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-20 08:39:06 +0530

    Modified : Adding Queue elements in the list

--- StackAndQueueList.java
+++ StackAndQueueList.java
@@ -2,21 +2,10 @@ public class StackAndQueueList {
     public static void main(String[] args) {
         System.out.println("Welcome to Stack And Queue List  Program");
         FunctionClass functionClass = new FunctionClass();
-        functionClass.insert(70);
-        functionClass.insert(30);
-        functionClass.insert(56);
+        functionClass.enQueue(56);
+        functionClass.enQueue(30);
+        functionClass.enQueue(70);
         functionClass.toPrint();
-        functionClass.peek();
-        functionClass.pop();
-        functionClass.toPrint();
-        functionClass.peek();
-        functionClass.pop();
-        functionClass.toPrint();
-        functionClass.peek();
-        functionClass.pop();
-        functionClass.toPrint();
-        functionClass.peek();
-        functionClass.pop();
 
     }
 }
